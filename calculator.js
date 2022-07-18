366f3f4b0df29b7e9e31b068d47dca78db45a9e6
Cassandra Lee <cassichan@Cassandras-Air.attlocal.net>
2022-07-17 21:57:36 -0400

    draft calculator project

---
+++ calculator.js
@@ -0,0 +1,31 @@
+//Turn on
+
+//Turn off
+
+
+//Function to add numbers. How to add many? pass input buttons through functions.
+const toAdd = (...input1, input2) => {
+    return input1 + input2;
+}
+
+//Function to subtract numbers
+const toSubtract = (...input1, input2) => {
+    return input1 - input2;
+}
+
+//Function to multiply
+const toMultiply = (...input1, input2) => {
+    return input1 * input2;
+}
+
+//Function to divide
+const toDivide = (...input1, input2) => {
+    return input1 / input2;
+}
+
+//Function to equal/submit
+
+//Function to change result screen
+
+//Function to clear results
+
